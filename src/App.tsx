96aaf2b2f31ff79e8c558876ae7dbe9c97d06c27
marrcelosantana <marcelosantana@alu.ufc.br>
2022-08-03 00:30:15 -0300

    init context

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import '../styles/global.scss';
+import { PokeContext } from './contexts/pokeContext';
 import { AppRoutes } from './routes';
 
 function App() {
   return (
     <div className="App">
-      <AppRoutes />
+      <PokeContext.Provider value={[]}>
+        <AppRoutes />
+      </PokeContext.Provider>
     </div>
   );
 }
