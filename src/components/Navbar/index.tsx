a7225897e7092d1f70fc0f26b58f1a40a3f957c6
marrcelosantana <marcelosantana@alu.ufc.br>
2022-11-04 13:14:03 -0300

    Update navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,66 +1,173 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import styles from './styles.module.scss';
 
 export function Navbar() {
+  const activeStyle = {
+    textDecoration: 'underline',
+    color: '#4d4d4d',
+    fontWeight: 'bold',
+  };
+
+  const defaultState = {
+    color: '#ababab',
+  };
+
   return (
     <nav className={styles.container}>
-      <Link to="/" className={styles.active}>
-        <a>Todos</a>
-      </Link>
-      <Link to="/bug-pokes">
-        <a>Inseto</a>
-      </Link>
-      <Link to="/dark-pokes">
-        <a>Sombrio</a>
-      </Link>
-      <Link to="/dragon-pokes">
-        <a>Dragão</a>
-      </Link>
-      <Link to="/electric-pokes">
-        <a>Elétrico</a>
-      </Link>
-      <Link to="/fairy-pokes">
-        <a>Fada</a>
-      </Link>
-      <Link to="/fighting-pokes">
-        <a>Lutador</a>
-      </Link>
-      <Link to="/fire-pokes">
-        <a>Fogo</a>
-      </Link>
-      <Link to="/flying-pokes">
-        <a>Voador</a>
-      </Link>
-      <Link to="/ghost-pokes">
-        <a>Fantasma</a>
-      </Link>
-      <Link to="/grass-pokes">
-        <a>Planta</a>
-      </Link>
-      <Link to="/ground-pokes">
-        <a>Terrestre</a>
-      </Link>
-      <Link to="/ice-pokes">
-        <a>Gelo</a>
-      </Link>
-      <Link to="/normal-pokes">
-        <a>Normal</a>
-      </Link>
-      <Link to="/psychic-pokes">
-        <a>Psíquico</a>
-      </Link>
-      <Link to="/rock-pokes">
-        <a>Pedra</a>
-      </Link>
-      <Link to="/steel-pokes">
-        <a>Aço</a>
-      </Link>
-      <Link to="/water-pokes">
-        <a>Água</a>
-      </Link>
-      <Link to="/poison-pokes">
-        <a>Venenoso</a>
-      </Link>
+      <ul>
+        <li>
+          <NavLink
+            to="/"
+            style={({ isActive }) => (isActive ? activeStyle : defaultState)}
+          >
+            <span>Todos</span>
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/bug-pokes"
+            style={({ isActive }) => (isActive ? activeStyle : defaultState)}
+          >
+            <span>Inseto</span>
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/dark-pokes"
+            style={({ isActive }) => (isActive ? activeStyle : defaultState)}
+          >
+            <span>Sombrio</span>
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/dragon-pokes"
+            style={({ isActive }) => (isActive ? activeStyle : defaultState)}
+          >
+            <span>Dragão</span>
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/electric-pokes"
+            style={({ isActive }) => (isActive ? activeStyle : defaultState)}
+          >
+            <span>Elétrico</span>
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/fairy-pokes"
+            style={({ isActive }) => (isActive ? activeStyle : defaultState)}
+          >
+            <span>Fada</span>
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/fighting-pokes"
+            style={({ isActive }) => (isActive ? activeStyle : defaultState)}
+          >
+            <span>Lutador</span>
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/fire-pokes"
+            style={({ isActive }) => (isActive ? activeStyle : defaultState)}
+          >
+            <span>Fogo</span>
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/flying-pokes"
+            style={({ isActive }) => (isActive ? activeStyle : defaultState)}
+          >
+            <span>Voador</span>
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/ghost-pokes"
+            style={({ isActive }) => (isActive ? activeStyle : defaultState)}
+          >
+            <span>Fantasma</span>
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/grass-pokes"
+            style={({ isActive }) => (isActive ? activeStyle : defaultState)}
+          >
+            <span>Planta</span>
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/ground-pokes"
+            style={({ isActive }) => (isActive ? activeStyle : defaultState)}
+          >
+            <span>Terrestre</span>
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/ice-pokes"
+            style={({ isActive }) => (isActive ? activeStyle : defaultState)}
+          >
+            <span>Gelo</span>
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/normal-pokes"
+            style={({ isActive }) => (isActive ? activeStyle : defaultState)}
+          >
+            <span>Normal</span>
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/psychic-pokes"
+            style={({ isActive }) => (isActive ? activeStyle : defaultState)}
+          >
+            <span>Psíquico</span>
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/rock-pokes"
+            style={({ isActive }) => (isActive ? activeStyle : defaultState)}
+          >
+            <span>Pedra</span>
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/steel-pokes"
+            style={({ isActive }) => (isActive ? activeStyle : defaultState)}
+          >
+            <span>Aço</span>
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/water-pokes"
+            style={({ isActive }) => (isActive ? activeStyle : defaultState)}
+          >
+            <span>Água</span>
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/poison-pokes"
+            style={({ isActive }) => (isActive ? activeStyle : defaultState)}
+          >
+            <span>Venenoso</span>
+          </NavLink>
+        </li>
+      </ul>
     </nav>
   );
 }
